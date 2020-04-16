cd90136ff18a48a50c498d2e0e30e909c12afe15
Troels Ugilt Jensen <tujensen@gmail.com>
2020-04-16 10:41:08 +0200

    ARMSO-3: Fixed table

--- Resources/public/apps/aarhusSecondTemplates/table-tool.js
+++ Resources/public/apps/aarhusSecondTemplates/table-tool.js
@@ -20,33 +20,67 @@ angular.module('aarhusSecondTemplates').directive('tableTool', [
         if (!scope.slide.options.table) {
           scope.slide.options.table = {
             title: '',
-            headers: [
-              '', '', '', ''
-            ],
-            rows: [
-              [
-                {
-                  type: 'date',
-                  value: null,
-                },
-                '',
-                '',
-                ''
-              ]
-            ],
+            headers: [],
+            rows: [],
           };
         }
 
+        scope.addColumn = function addColumn(type) {
+          type = (typeof type !== 'undefined') ? type : 'text';
+
+          var newHeader = {
+            type: type,
+            value: ''
+          };
+
+          scope.slide.options.table.headers.push(newHeader);
+
+          // @TODO: Add new column to existing rows.
+        };
+
+        scope.removeColumn = function removeColumn(index) {
+          // Remove column from headers.
+          scope.slide.options.table.headers = scope.slide.options.table.headers.filter(function (el, elIndex) {
+            return elIndex !== index;
+          });
+
+          // Remove column from each row.
+          for (var rowIndex in scope.slide.options.table.rows) {
+            var row = scope.slide.options.table.rows[rowIndex];
+
+            row = row.filter(function (el, elIndex) {
+              return elIndex !== index;
+            });
+          }
+        };
+
         scope.addRow = function addRow() {
-          // @TODO: variable columns.
-          scope.slide.options.table.rows.push(['','','','']);
+          var newRow = [];
+
+          // Add a new cell for each header cell, of the headers type.
+          for (var i = 0; i < scope.slide.options.table.headers.length; i++) {
+            var header = scope.slide.options.table.headers[i];
+            var newCell = {
+              type: header.type,
+              value: ''
+            };
+
+            newRow.push(newCell);
+          }
+
+          scope.slide.options.table.rows.push(newRow);
 
           console.log(scope.slide.options.table.rows);
         };
 
         scope.removeRow = function removeRow(index) {
-          console.log(index);
-          scope.slide.options.table.rows.splice(index, 1);
+          console.log('removing row: ' + index);
+
+          scope.slide.options.table.rows = scope.slide.options.table.rows.filter(function (el, elIndex) {
+            return elIndex !== index;
+          });
+
+          console.log(scope.slide.options.table.rows);
         };
       },
       templateUrl: '/bundles/itkaarhussecondtemplate/apps/aarhusSecondTemplates/table-tool.html'
